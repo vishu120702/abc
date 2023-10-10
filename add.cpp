8fd0168a290aed173b91488afb1b7515e49bd250
vishu120702 <vishutambekar120702@gmail.com>
2023-10-10 07:30:25 +0530

    asdc

--- add.cpp
+++ add.cpp
@@ -7,7 +7,7 @@ int main()
     int b = 23;
     int ans = a-b;
     cout << ans << endl;
-    // afyganrgfcoiauhiefilae
+    // afyganrgfcoiauhiefilaed
     // arlifuaoisdcaoieufhaoiueshfoiajsd oIEUFHAOIS JDFCAKJFSHEO IUseiuFHoli uHEFiuKJLSdfh ;SI efliuSlIUEFH liuef
     for(int i=0;i<10;i++){
         cout<<i;
