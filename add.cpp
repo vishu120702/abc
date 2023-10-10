d07973ff929a73299e748b91394f45f6ecaff1db
vishu120702 <vishutambekar120702@gmail.com>
2023-10-10 06:55:51 +0530

    :|

--- add.cpp
+++ add.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a=10;
-    int b=23;
-    int ans=b-a;
-    cout<<ans<<endl;
+int main()
+{
+    int a = 10;
+    int b = 23;
+    int ans = b - a;
+    cout << ans << endl;
+    // afyganrgfcoiauhiefilae
+    // arlifuaoisdcaoieufhaoiueshfoiajsd oIEUFHAOIS JDFCAKJFSHEO IUseiuFHoli uHEFiuKJLSdfh ;SI efliuSlIUEFH liuef
     return 0;
 }
